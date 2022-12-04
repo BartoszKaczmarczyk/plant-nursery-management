5e0992a563452749757b51dc565aab996b41d32c
Bartosz <bartosz.kaczmarczyk@icloud.com>
2022-12-04 21:32:35 +0100

    Initial commit

---
+++ src/test/java/pl/bartoszkaczmarczyk/plantnurserymanagement/PlantNurseryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartoszkaczmarczyk.plantnurserymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantNurseryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
